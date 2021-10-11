ccb695df160ca2a928abbbfe7d2bf89b63c0724f
L1ndseyHerman <lherman21@jcu.edu>
2021-10-10 22:42:35 -0400

    Caught up to vid372: got the "Favorites" page working again.

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,15 +1,17 @@
 import React, { useContext } from "react";
 
 import ProductItem from "../components/Products/ProductItem";
-import { ProductsContext } from "../context/products-context";
+import { useStore } from "../hooks-store/store";
 import "./Products.css";
 
 const Products = (props) => {
-  const productList = useContext(ProductsContext).products;
+  //const [state, dispatch] = useStore();
+  //  Only interested in the state here, so do like this:
+  const state = useStore()[0];
 
   return (
     <ul className="products-list">
-      {productList.map((prod) => (
+      {state.products.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
